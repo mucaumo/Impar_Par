c46f04a33eca967aac67a0ca88a4e1fa6a15d53d
mucaumo <murilo.caumo.murgia@gmail.com>
2024-03-21 09:28:03 -0300

    Layout feito

---
+++ app/src/main/java/com/example/impar_par/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.impar_par
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.impar_par.databinding.ActivityMainBinding
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
